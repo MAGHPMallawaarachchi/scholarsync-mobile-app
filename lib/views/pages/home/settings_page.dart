b472d7c25dd06f0172f9a13ea2f1834c596c8e01
Hasini Punsala <hasinipunsala@gmail.com>
2023-09-19 10:43:40 +0530

    feat: settings page

--- lib/views/pages/home/settings_page.dart
+++ lib/views/pages/home/settings_page.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:scholarsync/constants/icon_constants.dart';
 import 'package:scholarsync/themes/palette.dart';
-import 'package:scholarsync/views/widgets/custom_textfield.dart';
 import 'package:scholarsync/views/widgets/app_bar.dart';
-// import 'package:scholarsync/theme/app_theme.dart';
-// import 'package:scholarsync/features/view/home_page.dart';
+import 'widgets/settings_widget.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -19,171 +18,51 @@ class SettingsPage extends StatelessWidget {
         rightIcon: IconConstants.hamburgerMenuIcon,
         leftIcon: IconConstants.leftArrowIcon,
         leftIconToolTip: 'go to the next page',
-        onLeftIconPressed: () {},
+        onLeftIconPressed: () {
+          Navigator.pop(context);
+        },
         onRightIconPressed: () {},
       ),
       backgroundColor: PaletteLightMode.backgroundColor,
       //body
       body: Padding(
-        padding: const EdgeInsets.only(bottom: 20, left: 20, right: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'General',
-              style: TextStyle(
-                fontSize: 14,
-                fontFamily: "inter",
-                color: PaletteLightMode.titleColor,
-              ),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          const Text(
+            'General',
+            style: TextStyle(
+              fontSize: 16,
+              fontFamily: "inter",
+              fontWeight: FontWeight.w600,
+              color: PaletteLightMode.textColor,
             ),
-            const SizedBox(height: 10),
-            //settings widget 01
-
-            Container(
-              decoration: const BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: PaletteLightMode.shadowColor,
-                    offset: Offset(8, 8),
-                    blurRadius: 24,
-                    spreadRadius: 0,
-                  ),
-                ],
-              ),
-              child: Center(
-                child: CustomTextField(
-                  firstLine: "Receive Notifications",
-                  firstLineStyle: const TextStyle(
-                    fontSize: 14,
-                    color: PaletteLightMode.titleColor,
-                  ),
-                  controller: TextEditingController(),
-                  ontapBox: () {
-                    // onTap function for the Box
-                  },
-                  ontapleftIcon: () {
-                    // onTap function for leftIcon
-                  },
-                  ontaprightIcon: () {
-                    // onTap function for rightIcon
-                  },
-                  leftIcon: IconConstants.bellOutlinedIcon,
-                  rightIcon: IconConstants.toggleOffIcon,
-                  leftIconScale: 50,
-                  rightIconScale: 60,
-                  borderColor: Colors.transparent,
-                  borderWidth: 0,
-                  backgroundColor: PaletteLightMode.backgroundColor,
-                  boxwidth: 369.84,
-                  boxheight: 75,
-                  borderRadius: 10,
-                  padding: 16,
-                ),
-              ),
+          ),
+          const SizedBox(height: 20),
+          //settings widget 01
+          SettingsWidget(
+            leftIcon: PhosphorIcons.light.bell,
+            text: 'Receive Notifications',
+            onToggle: (value) {},
+            isSwitched: false,
+          ),
+          const SizedBox(height: 20),
+          const Text(
+            'Theme',
+            style: TextStyle(
+              fontSize: 16,
+              fontFamily: "inter",
+              fontWeight: FontWeight.w600,
+              color: PaletteLightMode.textColor,
             ),
-
-// Add spacing between the CustomTextField and the new Text widget
-
-            const SizedBox(height: 20),
-            const Text(
-              'Theme',
-              style: TextStyle(
-                fontSize: 14,
-                fontFamily: "inter",
-                color: PaletteLightMode.titleColor,
-              ),
-            ),
-//settings widget 02
-
-            Container(
-              decoration: const BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: PaletteLightMode.shadowColor,
-                    offset: Offset(8, 8),
-                    blurRadius: 24,
-                    spreadRadius: 0,
-                  ),
-                ],
-              ),
-              child: Center(
-                child: CustomTextField(
-                  firstLine: "Dark Mode",
-                  firstLineStyle: const TextStyle(
-                    fontSize: 14,
-                    color: PaletteLightMode.titleColor,
-                  ),
-                  controller: TextEditingController(),
-                  ontapBox: () {
-                    // onTap function for the Box
-                  },
-                  ontapleftIcon: () {
-                    // onTap function for leftIcon
-                  },
-                  ontaprightIcon: () {
-                    // onTap function for rightIcon
-                  },
-                  leftIcon: IconConstants.moonIcon,
-                  rightIcon: IconConstants.toggleOffIcon,
-                  leftIconScale: 35,
-                  rightIconScale: 60,
-                  borderColor: Colors.transparent,
-                  borderWidth: 0,
-                  backgroundColor: PaletteLightMode.backgroundColor,
-                  boxwidth: 369.84,
-                  boxheight: 75,
-                  borderRadius: 10,
-                  padding: 16,
-                ),
-              ),
-            ),
-            const SizedBox(height: 10),
-//settings widget 03
-            Container(
-              decoration: const BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color: PaletteLightMode.shadowColor,
-                    offset: Offset(8, 8),
-                    blurRadius: 24,
-                    spreadRadius: 0,
-                  ),
-                ],
-              ),
-              child: Center(
-                child: CustomTextField(
-                  firstLine: "Auto Dark Mode",
-                  firstLineStyle: const TextStyle(
-                    fontSize: 14,
-                    color: PaletteLightMode.titleColor,
-                  ),
-                  controller: TextEditingController(),
-                  ontapBox: () {
-                    // onTap function for the Box
-                  },
-                  ontapleftIcon: () {
-                    // onTap function for leftIcon
-                  },
-                  ontaprightIcon: () {
-                    // onTap function for rightIcon
-                  },
-                  leftIcon: IconConstants.darkNightModeIcon,
-                  rightIcon: IconConstants.toggleOffIcon,
-                  leftIconScale: 50,
-                  rightIconScale: 60,
-                  borderColor: Colors.transparent,
-                  borderWidth: 0,
-                  backgroundColor: Colors.white,
-                  boxwidth: 369.84,
-                  boxheight: 75,
-                  borderRadius: 10,
-                  padding: 16,
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+          const SizedBox(height: 20),
+          SettingsWidget(
+            leftIcon: PhosphorIcons.light.moon,
+            text: 'Dark Mode',
+            onToggle: (value) {},
+            isSwitched: false,
+          ),
+        ]),
       ),
     );
   }
---
+++ lib/views/pages/home/widgets/settings_widget.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import '../../../../themes/palette.dart';
+
+class SettingsWidget extends StatefulWidget {
+  final IconData leftIcon;
+  final bool isSwitched;
+  final String text;
+  final Function(bool) onToggle;
+
+  const SettingsWidget({
+    required this.leftIcon,
+    required this.isSwitched,
+    required this.text,
+    required this.onToggle,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<SettingsWidget> createState() => _SettingsWidgetState();
+}
+
+class _SettingsWidgetState extends State<SettingsWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(18)),
+        color: PaletteLightMode.whiteColor,
+        boxShadow: [
+          BoxShadow(
+            color: PaletteLightMode.shadowColor,
+            offset: Offset(8, 8),
+            blurRadius: 24,
+            spreadRadius: 0,
+          ),
+        ],
+      ),
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Icon(
+                widget.leftIcon,
+                size: 28.0,
+              ),
+              const SizedBox(width: 10),
+              Text(
+                widget.text,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  color: PaletteLightMode.textColor,
+                ),
+                textAlign: TextAlign.left,
+              ),
+            ],
+          ),
+          Switch(
+            value: widget.isSwitched,
+            onChanged: widget.onToggle,
+            activeTrackColor: PaletteLightMode.secondaryGreenColor,
+            activeColor: PaletteLightMode.secondaryGreenColor,
+          ),
+        ],
+      ),
+    );
+  }
+}
